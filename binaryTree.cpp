8a1a059bca7b5a281b45d7d26720345123af7a13
zhaoly12 <zhaoly12@163.com>
2020-07-31 18:16:12 +0800

    thread a binary tree

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -10,7 +10,8 @@ define a binary tree using linked list
 typedef struct node
 {
 	elemType data;
-	node* LChild, *RChild, *parent;	
+	node* LChild, *RChild, *parent;
+	int LTag, RTag;	
 } node, *tree;
  
 /*
@@ -322,6 +323,8 @@ void create(tree* t, tree parent)
     	(*t) = (node*)malloc(sizeof(node));
     	(*t)->data = data;
     	(*t)->parent = parent;
+    	(*t)->LTag = 0;
+    	(*t)->RTag = 0;
     	create(&((*t)->LChild), *t);
     	create(&((*t)->RChild), *t);
 	}
@@ -333,7 +336,222 @@ empty a binary tree
 void clear(tree* t)
 {
 	(*t) = NULL;
-	puts("tree cleared!");
+	puts("\ntree cleared!");
+}
+
+/*
+thread a binary tree
+*/
+// if a node does not have a left child LTag will be set to 1 and its left child pointer will point at its prior node
+// if a node does not have a right child RTag will be set to 1 and its right child pointer will point at its next node
+void thread(tree t, node** pre, char mode)
+{
+	// thread a tree in DLR sequence
+	if(mode == 'h')
+	{
+		if(t)
+		{
+			if(t->LChild == NULL && (*pre) != NULL)
+			{
+				t->LTag = 1;
+				t->LChild = (*pre);
+			}
+			if(*pre)
+			{
+				if(!(*pre)->RChild)
+				{
+					(*pre)->RTag = 1;
+					(*pre)->RChild = t;
+				}
+			}
+			*pre = t;
+			printf("->%c", (*pre)->data);
+			
+			// thread the left child tree
+			if(t->LTag != 1)
+				thread(t->LChild, pre, 'h');
+			// thread the right child tree
+			if(t->RTag != 1)
+				thread(t->RChild, pre, 'h');
+		}
+	}
+	// thread a tree in LDR sequence
+	else if (mode == 'c')
+	{
+		if(t)
+		{
+			// thread the left child tree
+			thread(t->LChild, pre, 'c');
+			
+			if(!t->LChild && (*pre) != NULL)
+			{
+				t->LTag = 1;
+				t->LChild = *pre;
+			}	
+			if(*pre)
+			{
+				if(!(*pre)->RChild)	
+				{
+					(*pre)->RTag = 1;
+					(*pre)->RChild = t;	
+				}	
+			}
+			*pre = t;
+			printf("->%c", (*pre)->data);
+			
+			// thread the right child tree
+			thread(t->RChild, pre, 'c');
+		}
+	}
+	// thread a tree in LRD sequence
+	else
+	{
+		if(t)
+		{
+			// thread the left child tree
+			thread(t->LChild, pre, 't');
+			// thread the right child tree
+			thread(t->RChild, pre, 't');
+			
+			if(t->LChild == NULL && (*pre) != NULL && t->LTag != 1)
+			{
+				t->LTag = 1;
+				t->LChild = *pre;
+			}	
+			if(*pre)
+			{
+				if((*pre)->RChild == NULL && (*pre)->RTag != 1)	
+				{
+					(*pre)->RTag = 1;
+					(*pre)->RChild = t;	
+				}	
+			}
+			*pre = t;
+			printf("->%c", (*pre)->data);
+		}
+	}	
+}
+
+/*
+find the prior node of a node in a threaded binary tree
+*/
+node* prior(node* n, char mode)
+{
+	node* pre = NULL;
+	if(n->LTag == 1)
+		pre = n->LChild;
+	else
+	{
+		// DLR 
+		// the right chid of the rightmost leaf node is NULL in DLR mode
+		// the left child of the root node is NULL in DLR mode when there is no left child tree
+		if(mode == 'h')
+		{
+			node* p = n->parent;
+			if(p)
+			{
+				if(n == p->LChild || (p->LChild == NULL))
+					 pre = p;
+				else
+				{
+					// this node is the right child and its parent node has a left child
+					// in this case the prior node is the last node of the left child tree in DLR mode(the rightmost leaf node of the left child tree)
+					pre = p->LChild;
+					while((pre->LTag == 0 && pre->LChild) || (pre->RTag == 0 && pre->RChild))
+					{
+						if(pre->RChild)
+							pre = pre->RChild;
+						else
+							pre = pre->LChild;
+					}		
+				}
+			}
+			else
+				pre = NULL;
+		}
+		// LDR
+		// the right chid of the rightmost leaf node is NULL in LDR mode
+		// the left child of the leftmost leaf node is NULL in LDR mode
+		else if(mode == 'c')
+		{
+			node* p = NULL;
+			// the most right node of the left child tree is the prior node(here is different from LRD and DLR. the node does not have to be a leaf node!)
+			if(n->LChild)
+				for(p=n->LChild;(p->RTag==0 && p->RChild);p=p->RChild);
+			pre = p;
+		}
+		// LRD
+		// the left child of the leftmost leaf node is NULL in LRD mode
+		// the right child of the root node is NULL in LRD mode when there is no right child tree
+		else
+		{
+			if(n->RTag == 0 && n->RChild)
+				pre = n->RChild;
+			else
+				pre = n->LChild;
+		}	
+	}
+
+	return pre;
+}
+
+/*
+find the next node of a node in a threaded binary tree
+*/
+node* next(node* n, char mode)
+{
+	node* next = NULL;
+	if(n->RTag == 1)
+		next = n->RChild;
+	else
+	{
+		// DLR 
+		if(mode == 'h')
+		{
+			if(n->LTag == 0 && n->LChild)
+				next = n->LChild;
+			else
+				next = n->RChild;
+		}
+		// LDR
+		else if(mode == 'c')
+		{
+			node* p = NULL;
+			// the most left node of the right child tree is the next node(here is different from LRD and DLR. the node does not have to be a leaf node!)
+			if(n->RChild)
+				for(p=n->RChild;(p->LTag==0 && p->LChild);p=p->LChild);
+			next = p;
+		}
+		// LRD
+		else
+		{
+			node* p = n->parent;
+			if(p)
+			{
+				if(n == p->RChild || (p->RChild == NULL))
+				{
+					next = p;		
+				}
+				else
+				{
+					// the node is the left child and its parent node has a right child
+					// the next node is the first node of the right child tree(the leftmost leaf node of the right child tree)
+					next = p->RChild;
+					while((next->LChild && next->LTag == 0) || (next->RTag == 0 && next->RChild))
+					{
+						if(next->LChild)
+							next = next->LChild;
+						else
+							next = next->RChild;
+					}
+				}
+			}
+			else
+				next = NULL;
+		}			
+	} 
+	
+	return next;
 }
 
 
@@ -378,6 +596,74 @@ int main()
 	traverse2((*t), 't');
 	printf("\n");
 	
+	// thread a binary tree
+	node** pt = (node**)malloc(sizeof(node*));
+	*pt = NULL;
+	node* p = *t;
+/*
+	puts("\nthread the tree in LDR sequence");
+	printf("LDR");
+	thread((*t), pt, 'c');	
+	
+	puts("\nfind the next node");
+	while(next(p,'c'))
+	{
+		printf("->%c", p->data);
+		p = next(p,'c');
+	}	
+	printf("->%c\n", p->data);
+	
+	puts("\nfind the prior node");
+	while(prior(p,'c'))
+	{
+		printf("->%c", p->data);
+		p = prior(p,'c');
+	}
+	printf("->%c\n", p->data);
+*/
+/*	
+	puts("\nthread the tree in DLR sequence");
+	printf("DLR");
+	thread((*t), pt, 'h');
+		
+	puts("\nfind the next node");
+	while(next(p,'h'))
+	{
+		printf("->%c", p->data);
+		p = next(p,'h');
+	}	
+	printf("->%c\n", p->data);
+	
+	puts("\nfind the prior node");
+	while(prior(p,'h'))
+	{
+		printf("->%c", p->data);
+		p = prior(p,'h');
+	}
+	printf("->%c\n", p->data);
+*/	
+	
+	puts("\nthread the tree in LRD sequence");
+	printf("LRD");
+	thread((*t), pt, 't');	
+	
+	puts("\nfind the prior node");
+	while(prior(p,'t'))
+	{
+		printf("<-%c", p->data);
+		p = prior(p,'t');
+	}
+	printf("<-%c\n", p->data);	
+	
+	puts("\nfind the next node");
+	while(next(p,'t'))
+	{
+		printf("->%c", p->data);
+		p = next(p,'t');
+	}	
+	printf("->%c\n", p->data);	
+
+
 	// clear this binary tree 
 	clear(t);	
 	
